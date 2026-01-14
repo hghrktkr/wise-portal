fef79d0e8ccb6a184d5a62e17e7040790b5bdf55
hghrktkr <147295919+hghrktkr@users.noreply.github.com>
2026-01-14 14:02:27 +0900

    feedback保存関数

--- wise-portal/app/phase/userAttemptsStorage.tsx
+++ wise-portal/app/phase/userAttemptsStorage.tsx
@@ -1,6 +1,7 @@
-import { UserExerciseAttempt } from "./userStorage";
+import { UserExerciseAttempt, UserFeedbackAttempt } from "./userStorage";
 
-const STORAGE_KEY: string = 'user_attempts';
+const STORAGE_KEY: string = 'user_exercise_attempts';
+const STORAGE_KEY_FEEDBACK: string = 'user_exercise_attempts';
 
 export function loadUserExerciseAttempts(): UserExerciseAttempt[] {
     try {
@@ -24,6 +25,35 @@ export function saveUserExerciseAttempt(updatedAttempt: UserExerciseAttempt) {
 export function getLatestUserExerciseAttemptByPhaseId(phaseId: string): UserExerciseAttempt | undefined {
     return loadUserExerciseAttempts().filter(ua => ua.phaseId === phaseId)
                                 .reduce<UserExerciseAttempt | undefined>((latest, current) => {
+                                    if(!latest) return current;
+                                    return latest.submittedAt > current.submittedAt
+                                        ? latest
+                                        : current;
+                                    }, undefined);
+}
+
+export function loadUserFeedbackAttempts(): UserFeedbackAttempt[] {
+    try {
+        const userAttemptsJson = localStorage.getItem(STORAGE_KEY_FEEDBACK);
+        if(!userAttemptsJson) return [];
+        return JSON.parse(userAttemptsJson) as UserFeedbackAttempt[];
+    } catch {
+        return [];
+    }
+}
+
+export function saveUserFeedbackAttempt(updatedAttempt: UserFeedbackAttempt) {
+    const currentUserAttempts = loadUserFeedbackAttempts();
+    const updatedUserAttempts = [
+            ...currentUserAttempts,
+            updatedAttempt
+    ];
+    localStorage.setItem(STORAGE_KEY_FEEDBACK, JSON.stringify(updatedUserAttempts));
+}
+
+export function getLatestUserFeedbackAttemptByPhaseId(phaseId: string): UserFeedbackAttempt | undefined {
+    return loadUserFeedbackAttempts().filter(ua => ua.phaseId === phaseId)
+                                .reduce<UserFeedbackAttempt | undefined>((latest, current) => {
                                     if(!latest) return current;
                                     return latest.submittedAt > current.submittedAt
                                         ? latest
--- wise-portal/app/phase/userStorage.tsx
+++ wise-portal/app/phase/userStorage.tsx
@@ -5,3 +5,12 @@ export interface UserExerciseAttempt {
     submittedAt: number;
     userAnswers: Record<string, {isCorrect: boolean}>;
 }
+
+export interface UserFeedbackAttempt {
+    attemptId: string;
+    phaseId: string;
+    isPerfect: boolean;
+    submittedAt: number;
+    rating: Record<string, {rate: 1 | 2 | 3}>;
+    comment: string;
+}
